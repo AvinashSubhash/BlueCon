5527a96ec91bdcb59823abf33be96daf8bf19a12
AvinashSubhash <avinashsubhash19@gmail.com>
2023-02-03 10:51:18 +0530

    temperory-pairing-successful

--- src/controls.py
+++ src/controls.py
@@ -1,10 +1,12 @@
 import subprocess
 import os
-devices=[]
-final_result=[]
-paired_devices=[]
-def ConnectDevice():
-    pass
+FINAL_DEVICE_ID=[]
+FINAL_DEVICE_NAMES=[]
+PAIRED_DEVICE_NAMES=[]
+PAIRED_DEVICE_ID=[]
+def ConnectDevice(macAddr):
+    connecting_status = subprocess.Popen(['bluetoothctl','connect',str(macAddr)],stdout=subprocess.PIPE)
+    return connecting_status
 
 def SearchDevices():
     os.system("timeout 5s bluetoothctl scan on > devices.txt")    
@@ -14,13 +16,18 @@ def SearchDevices():
     file_data = file_data[0][2:]
     for i in range(len(file_data)):
         file_data[i] = file_data[i].split(" ",3)
-        if file_data[i][2] not in devices:
-            devices.append(file_data[i][2])
-            final_result.append(file_data[i][3])
-    return final_result
+        if file_data[i][0]=="[[0;92mNEW[0m]" and file_data[i][2] not in FINAL_DEVICE_ID:
+            FINAL_DEVICE_ID.append(file_data[i][2])
+            FINAL_DEVICE_NAMES.append(file_data[i][3])
+    os.system("bluetoothctl scan off")
+    return [FINAL_DEVICE_NAMES,FINAL_DEVICE_ID]
 
 def PairDevice(macAddr):
     pairing_status = subprocess.Popen(['bluetoothctl','pair',str(macAddr)],stdout=subprocess.PIPE)
+    out, err = pairing_status.communicate()
+    out = out.decode()
+    if "Pairing successful" in out:
+        return ConnectDevice(macAddr)
     return pairing_status
 
 def PairedDevices():
@@ -29,14 +36,16 @@ def PairedDevices():
     out = out.decode().split("\n")[:-1]
     for i in range(len(out)):
         out[i] = out[i].split(" ",2)
-        paired_devices.append(out[i][1])
+        PAIRED_DEVICE_NAMES.append(out[i][1])
+        PAIRED_DEVICE_ID.append(out[i][0])
         out[i] = out[i][2]
     return out
 
 def UnpairDevices():
-    for device in paired_devices:
+    for device in PAIRED_DEVICE_NAMES:
         data = subprocess.Popen(['bluetoothctl','remove',device],stdout=subprocess.PIPE)
         data,err = data.communicate()
         print(data.decode())
-    while len(paired_devices)>0:
-        paired_devices.pop(0)
+    while len(PAIRED_DEVICE_NAMES)>0:
+        PAIRED_DEVICE_NAMES.pop(0)
+        PAIRED_DEVICE_ID.pop(0)
--- src/manage.py
+++ src/manage.py
@@ -3,25 +3,28 @@ import os,sys,select,time
 from inputimeout import inputimeout
 def PairDev():
     while True:
-        available_devices = controls.SearchDevices()
+        [available_devices,device_id] = controls.SearchDevices()
         os.system('clear')
         print("\nAvailable devices for pairing: \n")
         for i in range(len(available_devices)):
             sys.stdout.write("\r{0} - {1}\n".format(i+1,available_devices[i]))
         try:
-            option = inputimeout(prompt=">",timeout=4)
-            if option==-1:
-                return None
-            elif (option>=0 and option<len(available_devices)):
-                print("Selected: ",available_devices[option][3])
-                data = controls.PairDevice(available_devices[option][2])
-                out, err = data.communicate()
-                out = out.decode()
-                if "Pairing successful" in out:
-                    return True
-                return False
+            option = int(inputimeout(prompt=">",timeout=4))
         except Exception:
-            pass
+            continue
+        if option==-1:
+            return None
+        elif (option>=0 and option<len(available_devices)):
+            print("Selected: ",available_devices[option-1])
+            data = controls.PairDevice(device_id[option-1])
+            out, err = data.communicate()
+            out = out.decode()
+            print(out)
+            os.system("sleep 4")
+            if "Pairing successful" in out:
+                return True
+            return False
+
 
 
 def PairedDevices():
